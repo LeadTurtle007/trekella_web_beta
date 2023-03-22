bef1ba818b7dc60e485627d7fcff4e0b7ab5dbb9
Pratik Ekghare <51785907+LeadTurtle007@users.noreply.github.com>
2023-03-22 19:12:48 +0530

    home page images live

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ab367ab48a90a43474f0f748ec3984a7",
-"index.html": "37d58d074c2c5b5f2437daa55db2dfd9",
+"index.html": "f178db26362a0728d70ae5ab4891a60b",
 "/": "03b6b5898377afd335883dc969680013",
-"main.dart.js": "6827befb3dc679b38d729a16edd82e73",
+"main.dart.js": "57ab3ad483088075145198b5af422a3c",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
